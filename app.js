2883beeca3176cf15cd82657dc66d9630ad51b21
johnshumiston <johnshumiston@gmail.com>
2016-05-10 17:22:31 -0400

    update

--- app.js
+++ app.js
@@ -9,24 +9,45 @@ var Restaurant = require('./models/restaurant');
 var Activity = require('./models/activity');
 var db = require('./models/database');
 var Promise = require('bluebird');
+var path = require('path');
 
 
-// app.use('/', function(req, res, next){
-// 	res.send('hey what do you want');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/node_modules')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', path.join(__dirname, './templates'));
+app.use('/', require('./routes/index'));
+
+// app.get('/', function(req, res, next){
+// 	Promise.all([Hotel.findAll({}), Restaurant.findAll({}), Activity.findAll({})])
+// 	.then(function(pages){
+// 		res.json(pages);
+// 	});
 // });
 
-// app.use('/', require('./routes/index'));
-
-app.get('/', function(req, res, next){
-	Promise.all([Hotel.findAll({}), Restaurant.findAll({}), Activity.findAll({})])
-	.then(function(pages){
-		res.json(pages);
-	});
-});
+// app.get('/main', function(req, res, next){
+// 	Hotel.findAll({})
+// 	.then(function(hotel) {
+// 		res.render('main', {hotel: hotel})
+// 	})
+// 	.catch(next);
+// })
 
 app.listen(2421, function(req, res, next){
 	console.log('So essited for our big trip!');
 
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,34 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 // var models = require('../models');
-// var place = models.Place;
-// var activity = models.Activity;
-// var restaurant = models.Restaurant;
-// var hotel = models.Hotel;
-// module.exports = router;
+var place = require('../models/place');
+var activity = require('../models/activity');
+var restaurant = require('../models/restaurant');
+var hotel = require('../models/hotel');
+var $ = require("jquery");
+
+
+router.get('/main', function(req, res, next){
+	Promise.all([hotel.findAll({}), restaurant.findAll({}), activity.findAll({})])
+	.then(function(results) {
+		var hotel = results[0];
+		var restaurant = results[1];
+		var activity = results[2];
+		res.render('layout', {
+			hotels: hotel,
+			restaurants: restaurant,
+			activities: activity
+		});
+	})
+	.catch(next);
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
